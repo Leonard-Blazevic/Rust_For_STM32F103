0c44bc19ccc690d376d2439adfb6fe64428009b4
Leonard <leonard.blazevic@fer.hr>
2018-06-03 16:34:55 +0200

    Turning on leds using blynk.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use hal::serial::Serial;
 use cortex_m::asm;
 
 struct Package {
-    data: [u8; 3],
+    data: [u8; 2],
 }
 
 fn main() {
@@ -38,6 +38,11 @@ fn main() {
     let mut gpioc = p.GPIOC.split(&mut rcc.apb2);// => sabirnica na kojoj se nalazi user led
 
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    let mut tv = gpiob.pb6.into_push_pull_output(&mut gpiob.crl);
+    let mut air_conditioner = gpiob.pb7.into_push_pull_output(&mut gpiob.crl);
+    let mut lamp = gpiob.pb8.into_push_pull_output(&mut gpiob.crh);
+
 
     // USART1
     // let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
@@ -66,40 +71,58 @@ fn main() {
 
     let (mut tx, mut rx) = serial.split();
 
-    let mut package = Package { data: [0; 3] };// => inicijalizacija paketa kojeg šaljemo/primamo preko serija (sadrži 3 podatka)
+    let mut package = Package { data: [0; 2] };// => inicijalizacija paketa kojeg šaljemo/primamo preko serija (sadrži 2 podatka)
     let mut cnt = 0;
+    let mut led_state = 0;
 
     loop {
         /*for mut dataPoint in package.data.iter() {
             *dataPoint = block!(rx.read()).unwrap();
         }
-
         for dataPoint in package.data.iter() {
             block!(tx.write(*dataPoint)).ok();
         }*/
 
 
-        if(cnt < 3)// => prima 3 podatka
+        if(cnt < 2)// => prima 2 podatka
         {
             package.data[cnt] = block!(rx.read()).unwrap();
             cnt = cnt + 1;
         }
-        else// => nakon prijema 3 podatka, iste podatke ispisuje i ovisno o njima pali/gasi ledicu ili ne radi nista
+        else// => nakon prijema 2 podatka, iste podatke ispisuje i ovisno o njima pali/gasi ledicu ili ne radi nista
         {
             cnt = 0;
 
-            if(package.data[2] == b'n'){// => funkcije set_low i set_high su obrnute (set_low pali ledicu)
-                led.set_low();
+            if(package.data[0] == b'1'){// => funkcije set_low i set_high su obrnute (set_low pali ledicu)
+                if(package.data[1] == b'1'){
+                    lamp.set_high();
+                }
+                else {
+                    lamp.set_low();
+                }
             }
-            else if(package.data[2] == b'f'){
-                led.set_high();
+            else if(package.data[0] == b'2'){
+                if(package.data[1] == b'1'){
+                    tv.set_high();
+                }
+                else {
+                    tv.set_low();
+                }
+            }
+            else if(package.data[0] == b'3'){
+                if(package.data[1] == b'1'){
+                    air_conditioner.set_high();
+                }
+                else {
+                    air_conditioner.set_low();
+                }
             }
 
-            for i in 0..3 {
+            /*for i in 0..2 {
                 block!(tx.write(package.data[i])).ok();
             }
 
-            block!(tx.write(b'\n')).ok();
+            block!(tx.write(b'\n')).ok();*/
         }
     }
 }
